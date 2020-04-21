ac376b1c5300ad37243bf5d666df91b73cd11e61
marceloarmando <mcuellarr98@gmail.com>
2020-04-20 19:51:56 -0500

    Agregar archivos de proyecto.

---
+++ Lab04/Lab04/CarouselPage2.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace Lab04
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CarouselPage2 : CarouselPage
+    {
+        public CarouselPage2()
+        {
+            InitializeComponent();
+            btnClose.Clicked += async (sender, e) =>
+            {
+                await Navigation.PopAsync();
+            };
+        }
+    }
+}
